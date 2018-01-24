fefd306c953d110ae93d9a43a7422d960f46a64a
dannymorales <danny@gadaproductions.com>
2018-01-24 15:44:26 -0500

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,3 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBZzkhZFckBx5q9if8DVx7bOLYulJoyWWg",
-    authDomain: "danilomorales-191120.firebaseapp.com",
-    databaseURL: "https://danilomorales-191120.firebaseio.com",
-    projectId: "danilomorales-191120",
-    storageBucket: "danilomorales-191120.appspot.com",
-    messagingSenderId: "369637244754"
+
   };
